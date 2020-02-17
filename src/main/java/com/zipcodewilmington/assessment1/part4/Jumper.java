96ca3a4b42a374e121e3b90834feda501eb6ee7a
Leila Hsiao <lsiao@zipcodes-MacBook-Pro-6.local>
2020-02-17 13:32:15 -0500

    part4 and 5 finished

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -1,11 +1,13 @@
 package com.zipcodewilmington.assessment1.part4;
 
+import java.util.ArrayList;
+
 public class Jumper {
 
     /*
      * Complete the function below.
      */
     public int jumps(int k, int j) {
-        return -1;
+        return (k/j) + k%j;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -1,8 +1,44 @@
 package com.zipcodewilmington.assessment1.part5;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Palindrome {
 
     public Integer countPalindromes(String input){
-        return null;
+        Set<String> palindromes = new HashSet<>();
+        int counter = 0;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 1; j <= input.length(); j++) {
+                if (isPalindrome(input.substring(i, j))) {
+                    palindromes.add(input.substring(i, j));
+                    //System.out.println(input.substring(i, j));
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+    public boolean isPalindrome(String str){
+
+        // Pointers pointing to the beginning
+        // and the end of the string
+        int i = 0, j = str.length() - 1;
+
+        // While there are characters toc compare
+        while (i < j) {
+
+            // If there is a mismatch
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+
+            // Increment first pointer and
+            // decrement the other
+            i++;
+            j--;
+        }
+
+        // Given string is a palindrome
+        return true;
     }
 }
